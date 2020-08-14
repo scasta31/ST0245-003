b5d7b0ffd48d6c3cd4f974a8f9f4aec343f8a62c
scasta31 <68928348+scasta31@users.noreply.github.com>
2020-08-14 18:25:51 -0500

    Add files via upload

--- talleres/taller03/Taller3_Punto1.py
+++ talleres/taller03/Taller3_Punto1.py
@@ -5,18 +5,15 @@ Sebastian Castaño Orozco 201610054014
 Dennis Castrillón Sepúlveda 201610035014
 
 """
-def Hanoi (N):
-    Noriginal=N
-    Hanoi_int(N,Noriginal)
-    
-def Hanoi_int(N,Noriginal,a="Torre 1",b="Torre 2",c="Torre 3"):
+
+def Hanoi(N,a="Torre 1",b="Torre 2",c="Torre 3"):
 
     if N==1:
         print("Mover de "+a+" a "+c)
     else:
-        Hanoi_int(N-1,a,c,b)
-        Hanoi_int(1,a,b,c)
-        Hanoi_int(N-1,b,a,c)
+        Hanoi(N-1,a,c,b)
+        Hanoi(1,a,b,c)
+        Hanoi(N-1,b,a,c)
 
 "Test"
 
