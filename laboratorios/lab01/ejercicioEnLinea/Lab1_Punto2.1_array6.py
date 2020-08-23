8843e35cc244c2b2f72f354fda3e0eb55f50c519
scasta31 <68928348+scasta31@users.noreply.github.com>
2020-08-23 13:08:56 -0500

    Add files via upload

--- laboratorios/lab01/ejercicioEnLinea/Lab1_Punto2.1_array6.py
+++ laboratorios/lab01/ejercicioEnLinea/Lab1_Punto2.1_array6.py
@@ -8,20 +8,20 @@ Dennis Castrillón Sepúlveda 201610035014
 
 Given an array of ints, compute recursively if the array contains a 6.
 """
-def array6(array,initialposition):
-    return internalarray(array,initialposition,0)
+def array6(array):
+    return internalarray(array,0)
 
-def internalarray(array,initialposition,suma):
+def internalarray(array,suma):
     if len(array)==0:
         return (suma)
     else:
         if array[len(array)-1]==6:
             suma=suma+1
-        return internalarray(array[initialposition:len(array)-1],initialposition,suma)
+        return internalarray(array[0:len(array)-1],suma)
     
 
 "Test"                 
-print("El vector "+str([1,4])+" tiene "+str(array6([1,4],0))+" seis.")
-print("El vector "+str([1,4,6])+" tiene "+str(array6([1,4,6],0))+" seis.")
-print("El vector "+str([6,0,6,6,6])+" tiene "+str(array6([6,0,6,6,6],0))+" seis.")
-print("El vector "+str([6,-1,-4,-6,6])+" tiene "+str(array6([6,-1,-4,-6,6],0))+" seis.")
+print("El vector "+str([1,4])+" tiene "+str(array6([1,4]))+" seis.")
+print("El vector "+str([1,4,6])+" tiene "+str(array6([1,4,6]))+" seis.")
+print("El vector "+str([6,0,6,6,6])+" tiene "+str(array6([6,0,6,6,6]))+" seis.")
+print("El vector "+str([6,-1,-4,-6,6])+" tiene "+str(array6([6,-1,-4,-6,6]))+" seis.")
